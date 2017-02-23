ea31b5177c4278c3690c5c203eedba52c85e579f
RobertDQuant <Robertdquant@gmail.com>
2017-02-23 09:40:09 -0500

    Rob-Variables

---
+++ Rob_Variables/sketch.js
@@ -0,0 +1,13 @@
+function setup() {
+  createCanvas(600, 400);
+  background(250, 250, 100);
+}
+
+function draw() {
+  fill(250, 200, 200);
+  ellipse(mouseX, mouseY, 100, 100);
+}
+
+function mousePressed() {
+  background(250, 250, 100);
+}
