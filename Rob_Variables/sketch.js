fa3e2406344b06eb466353c029a83b1ddbbd3206
RobertDQuant <Robertdquant@gmail.com>
2017-02-24 14:54:08 -0500

    Update sketch.js

--- Rob_Variables/sketch.js
+++ Rob_Variables/sketch.js
@@ -1,13 +1,13 @@
 function setup() {
-  createCanvas(600, 400);
-  background(250, 250, 100);
+  createCanvas(600, 400); // this is the size of the canvas
+  background(250, 250, 100); // this is the color of the canvas
 }
 
 function draw() {
-  fill(250, 200, 200);
-  ellipse(mouseX, mouseY, 100, 100);
+  fill(250, 200, 200); // this is the color of the circle
+  ellipse(mouseX, mouseY, 100, 100); // the ellipse changes color as the mouse moves
 }
 
-function mousePressed() {
+function mousePressed() { // the background changes once the mouse is clicked
   background(250, 250, 100);
 }
